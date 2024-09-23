57a940f748deb7c5a33f9a4c3d234f2e72c3b137
Jorge O. Sandoval <george.omar@gmail.com>
2024-09-22 22:49:01 -0500

    Seeding Update

--- src/seeds/seeds.module.ts
+++ src/seeds/seeds.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SeedsService } from './seeds.service';
 import { UsersModule } from 'src/users/users.module';
+import { ArtistsModule } from 'src/artists/artists.module';
+import { SongsModule } from 'src/songs/songs.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ArtistsModule, SongsModule],
   providers: [SeedsService],
+  exports: [SeedsService],
 })
 export class SeedsModule {}
--- src/seeds/seeds.service.ts
+++ src/seeds/seeds.service.ts
@@ -1,34 +1,54 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { faker } from '@faker-js/faker';
-import { CreateUserDto } from 'src/users/dto/create-user.dto';
-import * as bcrypt from 'bcryptjs';
+import { ArtistsService } from 'src/artists/artists.service';
+import { SongsService } from 'src/songs/songs.service';
 
 @Injectable()
 export class SeedsService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly artistsService: ArtistsService,
+    private readonly songsService: SongsService,
+  ) {}
 
   async seed() {
     const response = await this.usersService.getAll(1, 1);
 
     if (response.totalItems === 0) {
-      const user = await this.user();
-      await this.usersService.create(user);
+      await this.user('admin');
+
+      const artist = await this.artist('artist');
+      await this.song([artist.id]);
     }
   }
 
-  private async user() {
-    const salt = await bcrypt.genSalt();
-    const password = await bcrypt.hash('p4ssw0d!', salt);
-
+  private async user(username: string) {
     const user = {
       firstName: faker.person.firstName(),
       lastName: faker.person.lastName(),
-      username: faker.internet.userName(),
-      password,
+      username,
+      password: 'p4ssw0d!',
       apiKey: false,
     };
 
-    return user as CreateUserDto;
+    return await this.usersService.create(user);
+  }
+
+  private async artist(username: string) {
+    const user = await this.user(username);
+    return await this.artistsService.create({
+      stageName: faker.music.artist(),
+      userId: user.id,
+    });
+  }
+
+  private async song(artistIds: number[]) {
+    return await this.songsService.create({
+      title: faker.music.songName(),
+      artistIds,
+      releasedDate: faker.date.past(),
+      duration: faker.date.recent(),
+    });
   }
 }
